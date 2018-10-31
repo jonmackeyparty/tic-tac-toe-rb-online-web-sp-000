1410b82d85acd092271b17c46c5b50eda7f0f66b
Jon Mackey <jon@jonmackey.party>
2018-10-31 15:47:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,7 +51,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board,index) == true && position_taken?(board, index) == false
-    move(board, index, value)
+    move(board, index, value = 'X')
     display_board(board)
   else turn(board)
   end
