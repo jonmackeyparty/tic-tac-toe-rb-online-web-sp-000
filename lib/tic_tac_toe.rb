658ac60e86d5d31ef808f1df8e6a879c188a7f17
Jon Mackey <jon@jonmackey.party>
2018-10-31 14:10:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -135,7 +135,7 @@ def play(board)
     turn(board)
     over?(board)
   end
-  if won?(board) == true
+  elsif won?(board) == true
     puts "Congratulations, '#{winner}'!"
   elsif draw?(board) == true
     puts "Cat's Game!"
