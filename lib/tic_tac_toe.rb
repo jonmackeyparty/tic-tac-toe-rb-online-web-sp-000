0f851647fee55a5bf68c7ef9607f5ea7680d3adc
Jon Mackey <jon@jonmackey.party>
2018-10-31 14:53:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -106,9 +106,7 @@ end
 
 #over? method 
 def over?(board)
-  if draw?(board) == true 
-    return true
-  elsif won?(board) == true
+  if draw?(board) == true || won?(board) == true
     return true
   else
     return false
